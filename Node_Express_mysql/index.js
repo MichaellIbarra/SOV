eb394f7c25b4684b3e425c38297637e99173abcb
Michaell Ibarra <131507781+MichaellIbarra@users.noreply.github.com>
2023-11-07 16:50:51 -0500

    completado

--- Node_Express_mysql/index.js
+++ Node_Express_mysql/index.js
@@ -13,30 +13,29 @@ conex.connect((err)=>{
         console.log('conexion exitosa');
     }
 });
-
 //MOSTRAR
-// conex.query('SELECT  * FROM actor', function(err,result, fields){
-//     if(err) throw err;
-//     result.forEach(result => {
-//         console.log(result);
-//     });
-// });
+conex.query('SELECT  * FROM actor', function(err,result, fields){
+    if(err) throw err;
+    result.forEach(result => {
+        console.log(result);
+    });
+});
 
 // INSERTAR
-/* conex.query('INSERT INTO actor (first_name, last_name) VALUES ("MICHAELL","IBARRA")', (err,result)=>{
+conex.query('INSERT INTO actor (first_name, last_name) VALUES ("MICHAELL","IBARRA")', (err,result)=>{
     if(err) throw err;
     console.log('Registrado Agregado', result)
-}); */
+});
 
 // ACTUALIZAR
-/* conex.query('UPDATE actor SET first_name = "Matichelo", last_name = "Martinez" WHERE actor_id=1 ', (err,result)=>{
+conex.query('UPDATE actor SET first_name = "Matichelo", last_name = "Martinez" WHERE actor_id=1 ', (err,result)=>{
     if(err) throw err;
     console.log('Registrado Actualizado', result)
-}) */
+})
 
 // ELIMINAR
-/* conex.query('DELETE FROM actor WHERE actor_id=1 ', (err,result)=>{
+conex.query('DELETE FROM actor WHERE actor_id=1 ', (err,result)=>{
     if(err) throw err;
     console.log('Registrado Eliminado', result)
-}) */
+})
 conex.end();
