50741411cf2befb34514564b0b65e9dc4bd153cb
Michaell Ibarra <131507781+MichaellIbarra@users.noreply.github.com>
2023-11-07 16:28:33 -0500

    COMPLETADO

--- Node_Express_mysql/index.js
+++ Node_Express_mysql/index.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql');
-
 const conex = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -15,12 +14,29 @@ conex.connect((err)=>{
     }
 });
 
-conex.query('SELECT  * FROM actor', function(err,result, fields){
-    if(err) throw err;
+//MOSTRAR
+// conex.query('SELECT  * FROM actor', function(err,result, fields){
+//     if(err) throw err;
+//     result.forEach(result => {
+//         console.log(result);
+//     });
+// });
 
-    result.forEach(result => {
-        console.log(result);
-        
-    });
-})
+// INSERTAR
+/* conex.query('INSERT INTO actor (first_name, last_name) VALUES ("MICHAELL","IBARRA")', (err,result)=>{
+    if(err) throw err;
+    console.log('Registrado Agregado', result)
+}); */
+
+// ACTUALIZAR
+/* conex.query('UPDATE actor SET first_name = "Matichelo", last_name = "Martinez" WHERE actor_id=1 ', (err,result)=>{
+    if(err) throw err;
+    console.log('Registrado Actualizado', result)
+}) */
+
+// ELIMINAR
+/* conex.query('DELETE FROM actor WHERE actor_id=1 ', (err,result)=>{
+    if(err) throw err;
+    console.log('Registrado Eliminado', result)
+}) */
 conex.end();
